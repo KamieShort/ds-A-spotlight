298f546dcd4ca5c3c7c9ee66ea85b5fbfbd62c83
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-22 11:34:21 -0700

    some accomplished

--- challenges/index.js
+++ challenges/index.js
@@ -69,3 +69,15 @@ console.log(addOrange(["blue", "blue", "blue"]));
 const addCat = addFirst("cat");
 console.log(addCat(["dog", "bird", "lizard"]));
 console.log(addCat(["lizard", "donkey", "whale"]));
+
+////
+function some(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    const num = callback(arr[i]);
+    if (num === true) return true;
+  }
+  return false;
+}
+
+console.log(some([1, 6, 5], (n) => n % 2 === 0));
+console.log(some([1, 7, 3], (n) => n % 2 === 0));
