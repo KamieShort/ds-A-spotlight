b6c6729dc0eed4b92e0e51af67c1cad6d36f1063
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-22 10:14:56 -0700

    filter challenge

--- challenges/index.js
+++ challenges/index.js
@@ -13,3 +13,22 @@ function map(arr, callback) {
 }
 
 console.log(newArray);
+
+//////challenge deux
+//create an empty array
+//loop through that array, grabbing the elements from the array
+//
+
+function filter(arr, callback) {
+  const array = [];
+  for (let i = 0; i < arr.length; i++) {
+    const num = callback(arr[i]);
+    if (num === true) {
+      array.push(arr[i]);
+    }
+  }
+  return array;
+}
+
+console.log(filter([2, 6, 5], (n) => n % 2 === 0));
+console.log("dog");
